cbb15075d4f935dddd3c53bc1338da4858917690
Drew <drewortone@gmail.com>
2020-01-11 12:16:01 -0500

    first baseline commit

---
+++ LSTM_Multivarite_Model.py
@@ -0,0 +1,221 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import tensorflow as tf
+
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+
+#Importing Data
+# zip_path = tf.keras.utils.get_file(
+#     origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+#     fname='jena_climate_2009_2016.csv.zip',
+#     extract=True)
+# csv_path, _ = os.path.splitext(zip_path)
+
+df = pd.read_csv(r'D:\Datasets\Amazon_Stock.csv')
+
+
+#Setting All Variables
+
+#Trainng Dataset will be 300000
+TRAIN_SPLIT = 700
+BATCH_SIZE = 256
+BUFFER_SIZE = 100
+EVALUATION_INTERVAL = 200
+EPOCHS = 10
+
+#Makes this reproduceable
+tf.random.set_seed(13)
+
+
+##
+def create_time_steps(length):
+  time_steps = []
+  for i in range(-length, 0, 1):
+    time_steps.append(i)
+  return time_steps
+
+
+#creats a function so all plot look the same
+def show_plot(plot_data, delta, title):
+  labels = ['History', 'True Future', 'Model Prediction']
+  marker = ['.-', 'rx', 'go']
+  time_steps = create_time_steps(plot_data[0].shape[0])
+  if delta:
+    future = delta
+  else:
+    future = 0
+  plt.title(title)
+
+  for i, x in enumerate(plot_data):
+    if i:
+      plt.plot(future, plot_data[i], marker[i], markersize=10,
+               label=labels[i])
+    else:
+      plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+  plt.legend()
+  plt.xlim([time_steps[0], (future+5)*2])
+  plt.xlabel('Time-Step')
+  return plt
+
+#This is where you determine what variables ot predict
+features_considered = ['open','high','low','volume','B2B20KAMARMB','momentum50','RSI7']
+features = df[features_considered]
+features.index = df['time_stamp']
+print(features.head())
+
+features.plot(subplots=True)
+plt.show()
+
+dataset = features.values
+data_mean = dataset.mean(axis=0)
+data_std = dataset.std(axis=0)
+
+dataset = (dataset-data_mean)/data_std
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+  data = []
+  labels = []
+
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
+
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i, step)
+    data.append(dataset[indices])
+
+    if single_step:
+      labels.append(target[i+target_size])
+    else:
+      labels.append(target[i:i+target_size])
+
+  return np.array(data), np.array(labels)
+
+#Used 720 observations
+past_history = 50
+future_target = 10
+STEP = 6
+
+x_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,
+                                                   TRAIN_SPLIT, past_history,
+                                                   future_target, STEP,
+                                                   single_step=True)
+x_val_single, y_val_single = multivariate_data(dataset, dataset[:, 1],
+                                               TRAIN_SPLIT, None, past_history,
+                                               future_target, STEP,
+                                               single_step=True)
+
+print ('Single window of past history : {}'.format(x_train_single[0].shape))
+
+
+train_data_single = tf.data.Dataset.from_tensor_slices((x_train_single, y_train_single))
+train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_single = tf.data.Dataset.from_tensor_slices((x_val_single, y_val_single))
+val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
+
+
+single_step_model = tf.keras.models.Sequential()
+single_step_model.add(tf.keras.layers.LSTM(32,
+                                           input_shape=x_train_single.shape[-2:]))
+single_step_model.add(tf.keras.layers.Dense(1))
+
+single_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='mae')
+
+for x, y in val_data_single.take(1):
+  print(single_step_model.predict(x).shape)
+
+single_step_history = single_step_model.fit(train_data_single, epochs=EPOCHS,
+                                            steps_per_epoch=EVALUATION_INTERVAL,
+                                            validation_data=val_data_single,
+                                            validation_steps=50)
+
+def plot_train_history(history, title):
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  epochs = range(len(loss))
+
+  plt.figure()
+
+  plt.plot(epochs, loss, 'b', label='Training loss')
+  plt.plot(epochs, val_loss, 'r', label='Validation loss')
+  plt.title(title)
+  plt.legend()
+
+  plt.show()
+
+plot_train_history(single_step_history,
+                   'Single Step Training and validation loss')
+
+for x, y in val_data_single.take(3):
+  plot = show_plot([x[0][:, 1].numpy(), y[0].numpy(),
+                    single_step_model.predict(x)[0]], 12,
+                   'Single Step Prediction')
+  plt.show()
+
+
+future_target = 72
+x_train_multi, y_train_multi = multivariate_data(dataset, dataset[:, 1], 0,
+                                                 TRAIN_SPLIT, past_history,
+                                                 future_target, STEP)
+x_val_multi, y_val_multi = multivariate_data(dataset, dataset[:, 1],
+                                             TRAIN_SPLIT, None, past_history,
+                                             future_target, STEP)
+
+print ('Single window of past history : {}'.format(x_train_multi[0].shape))
+print ('\n Target Price to predict : {}'.format(y_train_multi[0].shape))
+
+train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
+train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
+val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
+
+def multi_step_plot(history, true_future, prediction):
+  plt.figure(figsize=(12, 6))
+  num_in = create_time_steps(len(history))
+  num_out = len(true_future)
+
+  plt.plot(num_in, np.array(history[:, 1]), label='History')
+  plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
+           label='True Future')
+  if prediction.any():
+    plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
+             label='Predicted Future')
+  plt.legend(loc='upper left')
+  plt.show()
+
+
+for x, y in train_data_multi.take(1):
+  multi_step_plot(x[0], y[0], np.array([0]))
+
+multi_step_model = tf.keras.models.Sequential()
+multi_step_model.add(tf.keras.layers.LSTM(32,
+                                          return_sequences=True,
+                                          input_shape=x_train_multi.shape[-2:]))
+multi_step_model.add(tf.keras.layers.LSTM(16, activation='relu'))
+multi_step_model.add(tf.keras.layers.Dense(72))
+
+multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mse')
+
+
+for x, y in val_data_multi.take(1):
+  print (multi_step_model.predict(x).shape)
+
+multi_step_history = multi_step_model.fit(train_data_multi, epochs=EPOCHS,
+                                          steps_per_epoch=EVALUATION_INTERVAL,
+                                          validation_data=val_data_multi,
+                                          validation_steps=50)
+
+plot_train_history(multi_step_history, 'Multi-Step Training and validation loss')
+
+for x, y in val_data_multi.take(3):
+  multi_step_plot(x[0], y[0], multi_step_model.predict(x)[0])
